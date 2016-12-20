a078b2be7074f1d82c0111bdf040a9d80631afef
EvGeniyStadnik <evgeniy.stadnik@gmail.com>
2016-12-20 19:06:21 +0200

    Add links to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,19 @@ var React = require('react');
 var About = (props) => {
     return (
       <div>
-        <h3>About</h3>
-        <p>Welcome to About page!</p>
+        <h1 className='text-center'>About</h1>
+        <p>Welcome to About page! That links are using for rich the sorces that was usd in project</p>
+        <ul>
+          <li>
+            <a href="https://devcenter.heroku.com/">Heroku</a> - This is for deploy to live web page
+          </li>
+          <li>
+            <a href="http://foundation.zurb.com/">Foundation</a> - This is for styling tamplates
+          </li>
+          <li>
+            <a href="http://openweathermap.org/">Openweathermap</a> - This is our weater
+          </li>
+        </ul>
       </div>
     )
 }
